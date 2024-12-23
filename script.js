ef8764049f1b273f27da2805e9ea6226bfd9b4dc
profbruuno <96679152+profbruuno@users.noreply.github.com>
2024-12-23 15:24:40 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,12 @@
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyCetRQqTMEVaoNjMZal8c02OzwtR6QqbPw",
-  authDomain: "tonminer-5f14c.firebaseapp.com",
-  projectId: "tonminer-5f14c",
-  storageBucket: "tonminer-5f14c.firebasestorage.app",
-  messagingSenderId: "313508121395",
-  appId: "1:313508121395:web:679ca9cf1148e9471b8c46",
-  measurementId: "G-EGBCB1L7PR"
-};
-
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-
 document.addEventListener("DOMContentLoaded", function() {
     var form = document.getElementById("loginForm");
 
     form.addEventListener("submit", function(event) {
         event.preventDefault();  // Prevent form submission
-        console.log("Form submitted"); // Debug message
 
         var email = document.getElementById("email").value;
         var password = document.getElementById("password").value;
         var errorMessage = document.getElementById("errorMessage");
-
-        console.log("Email: " + email); // Debug message
-        console.log("Password: " + password); // Debug message
 
         firebase.auth().signInWithEmailAndPassword(email, password)
             .then((userCredential) => {
